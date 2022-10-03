f86cbb028b4bb9d618e359b2d65361b511198c1a
Andreas Hald Søndergaard Mandøe <amandoee@gmail.com>
2022-10-03 19:28:26 +0200

    asda

--- sci.js
+++ sci.js
@@ -2,11 +2,12 @@
 
 function test() {
     alert("test");
-
-
-    document.write("<h1>Lolleren</h1>");
-    <h1>Test</h1>
-
+    <iframe id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="300"
+    height="200"
+    src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+</iframe>
 }
 //
 
