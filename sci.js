da012d54db43883b6fdf0555a1f087a65ac6b5f6
Andreas Hald Søndergaard Mandøe <amandoee@gmail.com>
2022-10-03 19:32:21 +0200

    ka

--- sci.js
+++ sci.js
@@ -1,8 +1,7 @@
-var url = "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik";
 
 function test() {
     alert("test");
-    
+    var url = "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik";
 }
 //
 
