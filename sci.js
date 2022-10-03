4fa0bc26692338322b419ec00ae4a6f96e3ef1b1
Andreas Hald Søndergaard Mandøe <amandoee@gmail.com>
2022-10-03 19:23:51 +0200

    asdasda

--- sci.js
+++ sci.js
@@ -3,15 +3,7 @@ function test() {
     alert("test");
 
 
-    document.write('\
-\
-    <h1>Add your HTML code here</h1>\
-\
-     <p>Notice however, that you have to escape LF's with a '\', just like\
-        demonstrated in this code listing.\
-    </p>\
-\
-');
+    document.write("<h1>Lolleren</h1>");
 
 
 }
